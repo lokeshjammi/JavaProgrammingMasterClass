c64655c49f7d72d3e4d83db5219969401617ae2a
lokeshjammi <jammilokesh3@gmail.com>
2022-07-08 19:55:40 +0530

    Method code commit

---
+++ src/Method/Main.java
@@ -0,0 +1,20 @@
+package Method;
+
+public class Main {
+    public static void main(String[] args){
+        calculateScore();
+    }
+
+    public static void calculateScore(){
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score is: "+finalScore);
+        }
+    }
+}
